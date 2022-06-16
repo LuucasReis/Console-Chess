904063f599d7504ea605dea943b3d32399d8521e
Lucas Reis <luke.spqr@gmail.com>
2022-06-16 20:41:50 -0300

    Starting implementation the board

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using tabuleiro;
 namespace Chess_Console
 {
     public class Program
     {
-        
+        static void Main(string[] args)
+        {
+            Tabuleiro x = new Tabuleiro(8,8);
+
+        }
     }
 }
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+namespace tabuleiro
+{
+    public class Peca
+    {
+        public Posicao Posicao {get; set;}
+        public Cor Cor {get; protected set;}
+        public int QuantidadeMov {get; protected set;}
+        public Tabuleiro Tab {get; protected set;}
+
+        public Peca()
+        {
+        }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+            QuantidadeMov = 0;
+        }
+    }
+}
